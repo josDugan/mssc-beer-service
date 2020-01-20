b35b1c4b12610c5da8d7e6f445b959cf33b2889d
josDugan <josephdug@gmail.com>
2020-01-19 20:50:10 -0500

    Update unit tests

--- src/test/java/guru/springframework/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/web/controller/BeerControllerTest.java
@@ -1,22 +1,30 @@
 package guru.springframework.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.domain.Beer;
+import guru.springframework.respositories.BeerRepository;
 import guru.springframework.web.model.BeerDto;
 import guru.springframework.web.model.BeerStyle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "guru.springframework.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -24,6 +32,9 @@ class BeerControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    BeerRepository beerRepository;
 
     BeerDto beerDto;
 
@@ -39,6 +50,8 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON)
         )
