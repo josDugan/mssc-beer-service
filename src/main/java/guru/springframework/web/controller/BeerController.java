ed3fbae36dcdf04bcb47447b019e46d77b688ec6
josDugan <josephdug@gmail.com>
2020-01-19 20:42:32 -0500

    Integrate controller and repository

--- src/main/java/guru/springframework/web/controller/BeerController.java
+++ src/main/java/guru/springframework/web/controller/BeerController.java
@@ -1,6 +1,9 @@
 package guru.springframework.web.controller;
 
+import guru.springframework.mappers.BeerMapper;
+import guru.springframework.respositories.BeerRepository;
 import guru.springframework.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,25 +11,36 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-        BeerDto beerDto = BeerDto.builder().build();
-
-        return new ResponseEntity<>(beerDto, HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
+
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUpc());
 
+            beerRepository.save(beer);
+        });
     }
 }
