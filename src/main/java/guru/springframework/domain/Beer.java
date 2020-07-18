dcdb61e0ec3d3682eef4c2e2e40848eda6428e88
josDugan <josephdug@gmail.com>
2020-07-18 19:14:56 -0400

    Add local mysql config. Closes #22.

--- src/main/java/guru/springframework/domain/Beer.java
+++ src/main/java/guru/springframework/domain/Beer.java
@@ -22,8 +22,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
-    @Type(type = "uuid-char")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @Type(type = "org.hibernate.type.UUIDCharType")
     private UUID id;
 
     @Version
