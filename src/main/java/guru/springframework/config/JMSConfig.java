d1f1896893616d6871a68d110f3aeafbdd8e512a
josDugan <josephdug@gmail.com>
2020-07-28 23:07:39 -0400

    Add JMS and artemis config. Closes #24.

---
+++ src/main/java/guru/springframework/config/JMSConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+
+    public static final String BEER_SERVICE_QUEUE = "beer-service-queue";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
